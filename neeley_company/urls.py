8bf39e388e4f303e250c376556373fbdafdfc929
oleg <olegsh-gusev@mail.ru>
2021-01-12 15:17:31 +0300

    testindex

--- neeley_company/urls.py
+++ neeley_company/urls.py
@@ -20,8 +20,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("projects/", include("projects.urls")),
-    path("blog/", include("blog.urls")),
     path('', views.index, name='index')
     # path("", include("projects.urls")),
 ]
