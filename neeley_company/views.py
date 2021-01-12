ec21c1aec18f2be4ceca3b8722e0ffc4923b81f0
oleg <olegsh-gusev@mail.ru>
2021-01-12 14:50:39 +0300

    testindex

--- neeley_company/views.py
+++ neeley_company/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('django:RANDOMNESS_PLACEHOLDER')
+    context = {}
+    return render(request, 'index.html', context)
